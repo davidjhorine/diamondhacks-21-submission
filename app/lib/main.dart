af00456cde344476a6a1a0b79d391b44880eaa7f
David Horine <davidjhorine@protonmail.com>
2021-03-27 10:17:55 -0400

    Create a basic flutter template app

---
+++ app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'App title here',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(title: Text('Hello World!')),
+      body: Center(child: Text('Template project')),
+    ));
+  }
+}
