722268c7067cc78fdc21e5245c3f24ca45ec532f
xarvatium <csfore2003@protonmail.com>
2021-03-27 19:16:27 -0400

    Got the API in a semi-functional on a website

--- api/app.py
+++ api/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-from models import db
-from models import login
+from flask import Flask, render_template, redirect, request, jsonify
+from models import UserModel, db, login
 from flask_security import login_required
 from flask_login import current_user, login_user, logout_user
 
 app = Flask(__name__)
+
+app.secret_key = b'&t4@Q'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,21 +30,6 @@ def index():
 def blog():
     return render_template('test.html')
 
-# Login route
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    # Checks for if the user is already authenticated
-    if current_user.is_authenticated:
-        return redirect('/blogs')
-    
-    # Checks for if the method of request is a POST
-    if request.method == "POST":
-        email = request.form['email']
-        user = UserModel.query.filter_by(email = email).first()
-        if user is not None and user.check_password(request.form['password']):
-            login_user(user)
-            return redirect('/blogs')
-
 # Register route
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -55,7 +41,7 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        if UserModel.query.filter_by(email=email):
+        if UserModel.query.filter_by(email=email).first():
             return ('Email already present')
         
         user = UserModel(email=email, username=username)
@@ -66,11 +52,6 @@ def register():
     
     return render_template('register.html')
 
-# Logout route
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect('/blogs')
 
 # USER / AUTH API Requests
 # These are requests that retain directly to administering users
@@ -89,6 +70,13 @@ def login():
         if user is not None and user.check_password(request.form['password']):
             login_user(user)
             return redirect('/blogs')
+    return render_template('login.html')
+
+# Logout route
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/blogs')
 
 # Set Password for authed user
 @app.route('/auth/setPassword', methods=['POST'])
@@ -132,20 +120,44 @@ def api_addData():
 def api_getData():
     pass
 
-# edit data from trip for authed user
+# Edit data from trip for authed user
 @app.route('/data/edit', methods=['POST'])
 def api_editData():
     pass
 
-# delete data from trip for authed user
-@app.route('/data/delete', methods=['POST'])
-def api_deleteData():
-    pass
+# Delete data from trip for authed user
+#@app.route('/data/delete', methods=['POST'])
+#def api_deleteData():
+#    pass
 
 # COMPETITORS Requests
 # Get info on competitors to compare locally
 
+# List competitors for authed user
+@app.route('/competitors/listd', methods=['GET'])
+def api_listCompetitors():
+    pass
+
+# Add competitor for authed user
+@app.route('/competitors/add', methods=['POST'])
+def api_addCompetitor():
+    pass
+
+# Remove competitor for authed user
+@app.route('/competitors/remove', methods=['POST'])
+def api_removeCompetitor():
+    pass
+
+# MISCELLANEOUS Requests
+# This is for all requests that didn't fit into a certain category
+
+# STATS Request
+@app.route('/stats', methods=['GET'])
+def api_stats():
+    return True
+
+
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
